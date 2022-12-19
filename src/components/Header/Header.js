92d8f75384aa98a77e8a686dd7894ac91ab81cd2
touhidcodes <touhidcodes@gmail.com>
2022-12-19 23:09:56 +0600

    ema-jhon Header nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,15 @@
 import React from "react";
-
+import logo from "../../images/logo.png";
+import "./Header.css";
 const Header = () => {
   return (
-    <div>
-      <p>Hello World</p>
+    <div className="header">
+      <img src={logo} alt="" />
+      <nav>
+        <a href="/shop">Shop</a>
+        <a href="/review">Order Review</a>
+        <a href="/manage">Manage Inventory</a>
+      </nav>
     </div>
   );
 };
