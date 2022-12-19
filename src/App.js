b0cad29c97b7426b4a4684df97ecb87fed60e2cb
touhidcodes <touhidcodes@gmail.com>
2022-12-19 21:56:30 +0600

    ema-jhon Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  );
 }
 
 export default App;
